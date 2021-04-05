d9290454996c80eb408013234adef231090a0fa8
Tanish Agarwal <tanishagarwal81201@gmail.com>
2021-04-05 19:27:18 +0530

    frontend initialised

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import axios from "axios";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Header from "./components/Header";
+import Home from "./components/homepage";
+import Register from "./components/register";
+import Login from "./components/login";
+import Header from "./components/header";
 
 function App() {
   return (
     <BrowserRouter>
-      <UserContext.Provider value={{ userData, setUserData }}>
-        <Header />
-        <Switch>
-          <Route exact patch="/" component={Home} />
-          <Route patch="/register" component={Register} />
-          <Route patch="/login" component={Login} />
-        </Switch>
-      </UserContext.Provider>
+      <Header />
+      <Switch>
+        <Route exact patch="/" component={Home} />
+        <Route patch="/register" component={Register} />
+        <Route patch="/login" component={Login} />
+      </Switch>
     </BrowserRouter>
   );
 }
+//      <UserContext.Provider value={{ userData, setUserData }}>
 
 export default App;
